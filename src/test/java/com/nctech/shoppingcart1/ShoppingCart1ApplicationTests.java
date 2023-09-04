0500549b90e493ef105af041dc2a18d16ad5e72e
pazavur <pazavurdapirilas@gmail.com>
2023-09-04 19:08:08 +0530

    Initial commit

---
+++ src/test/java/com/nctech/shoppingcart1/ShoppingCart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nctech.shoppingcart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
